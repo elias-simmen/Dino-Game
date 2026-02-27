4efa46974109d4a7562c80e583331f13c1be037b
elias-simmen <elias_simmen@sluz.ch>
2026-02-27 11:22:15 +0100

    Add dino game files

---
+++ script.js
@@ -0,0 +1,33 @@
+const dino = document.getElementById("dino")
+const rock = document.getElementById("rock")
+const score = document.getElementById("score")
+
+function jump() {
+  dino.classList.add("jump-animation")
+  setTimeout(() => dino.classList.remove("jump-animation"), 750)
+}
+
+document.addEventListener("keypress", (event) => {
+  if (!dino.classList.contains("jump-animation")) {
+    jump()
+  }
+})
+
+setInterval(() => {
+  const dinoTop = parseInt(
+    window.getComputedStyle(dino).getPropertyValue("top"),
+  )
+  const rockLeft = parseInt(
+    window.getComputedStyle(rock).getPropertyValue("left"),
+  )
+  score.innerText = parseInt(score.innerText) + 2;
+
+  if (rockLeft < 0) {
+    rock.style.display = "none"
+  } else {
+    rock.style.display = ""
+  }
+
+  if (rockLeft < 50 && rockLeft > 0 && dinoTop > 150) 
+  {score.innerText = 0}
+}, 50)
